b1fdc9dd0015928316b874dedbf696bcb60855a5
Rodrigo <joserodrigoguimaraes160401@gmail.com>
2025-11-03 16:56:23 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import FarmaciaPage from '../src/page/FarmaciaPage';
 import LoginPage from '../src/page/login_page';
+import LayoutPrincipal from "./components/LayoutPrincipal";
+
+
 
 import LayoutProtegido from "./components/layout_protegido";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App(){
   return(
     <BrowserRouter>
+    <ToastContainer autoClose={300} hideProgressBar />
       <Routes>
 
-        <Route path='/login' element={<LoginPage />}/>
-     
+        <Route element={<LayoutPrincipal />}>
 
-        <Route element={<LayoutProtegido/>}>
-          <Route path='/' element={<FarmaciaPage/>}/>
-        </Route>  
+          <Route path="/login" element={<LoginPage/>}/>     
+
+          <Route element={<LayoutProtegido/>}>
+            <Route path='/' element={<FarmaciaPage/>}/>
+          </Route>  
+
+        </Route>
+        
        </Routes>
         {/* <Route element={<Pagina_perfil/>}/> */}
       
