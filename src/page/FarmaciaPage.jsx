3017b9d1ca85abb94102ee43e6f1d2b0241edc23
Rodrigo <joserodrigoguimaraes160401@gmail.com>
2025-11-03 16:56:29 -0300

    Update FarmaciaPage.jsx

--- src/page/FarmaciaPage.jsx
+++ src/page/FarmaciaPage.jsx
@@ -8,22 +8,42 @@ import { useNavigate } from 'react-router-dom';
 //Importa CSS
 import '../App.css';
 
+import { toast } from 'react-toastify';
+
 const API_URL = 'https://69010550ff8d792314bc5118.mockapi.io/farmacia_api';
 
 
 
 function Farmacia_page() {
 
-  const navigate = useNavigate;
+  const navigate = useNavigate();
   // --- 1. ESTADO ---
   const [remedios, set_remedios] = useState([]);
   const [pedidos, set_pedidos] = useState([]); 
   const [abrir_modal, modal_aberto] = useState(false);
   const [remedio_selecionado, remedio_definido] = useState(null);
 
+  const [confirmar, set_confirmar] = useState(false);
+  const [deleta, set_deleta] = useState(null);
+
+  //Guardar Perfil
+   const [perfil, set_perfil] = useState(null);
+
+   //Guardar email
+   const [email_logado, set_email_logado] = useState(null);
+
   
   // --- 2. EFEITO (BUSCAR DADOS INICIAIS) ---
   useEffect(() => {
+
+    //ler o perfil do usuário
+
+    const perfil_salvo = localStorage.getItem('perfil_usuario');
+    set_perfil(perfil_salvo);
+
+    //ler o email do usuário
+    const email_salvo = localStorage.getItem('email_usuario');
+    set_email_logado(email_salvo);
     
     // (Função para buscar remédios)
     async function buscar_remedios() {
@@ -52,7 +72,12 @@ function Farmacia_page() {
     }
 
     buscar_remedios();
-    buscar_pedidos();
+
+    //só busca pedidos se for gerente
+    if(perfil_salvo === 'gerente' || perfil_salvo === 'cliente'){
+      buscar_pedidos();
+    }
+
     
   }, []); //[] diz para a função rodar só uma vez
 
@@ -62,7 +87,7 @@ function Farmacia_page() {
   //Abrir modal
   const abrir_form = (remedio) => {
     if (remedio.quantidade_estoque <= 0) {
-      alert('Desculpe, este item está fora de estoque');
+      toast.warn('Desculpe, este item está fora de estoque');
       return;
     }
     console.log('Selecionado:', remedio);
@@ -89,15 +114,12 @@ function Farmacia_page() {
       });
     } catch (error) {
       console.error('Erro ao atualizar o estoque:', error);
-      alert('Erro: O pedido foi criado, mas falhámos ao atualizar o estoque.');
+      toast.error('Erro: O pedido foi criado, mas falhámos ao atualizar o estoque.');
     }
   }
 
-  const logout = () =>{
-    localStorage.removeItem('perfil_usuario');
-    navigate('/login')
-  };
 
+  
   //SUBMIT FORM (CREATE & UPDATE)
   
   const criar_pedido = async (event) => {
@@ -132,7 +154,7 @@ function Farmacia_page() {
       await atualizar_estoque_api(remedio_id, novo_estoque);
 
       // 3. ATUALIZA A TELA 
-      alert('PEDIDO REALIZADO COM SUCESSO!');
+      toast.success('PEDIDO REALIZADO COM SUCESSO!');
       fechar_form();
       
       // Adiciona o novo pedido à lista de pedidos
@@ -145,26 +167,34 @@ function Farmacia_page() {
       
     } catch (error) {
       console.error('Erro no POST do pedido:', error);
-      alert('Falha ao realizar pedido. Tente novamente');
+      toast.error('Falha ao realizar pedido. Tente novamente');
     }
   };
   
   //DELETAR
-  const deletar_pedido = async (pedido) => {
-    console.log(`Cancelando pedido ${pedido.id}...`);
-
-    const confirmando = confirm('Tem certeza que deseja cancelar esse pedido? O item voltará ao estoque.');
-    if (!confirmando) return;
-
-    // 1. Encontra o remédio correspondente na lista de remédios
-    const remedio_correspondente = remedios.find(
-      (r) => r.name === pedido.remedio_nome
-    );
+  const deletar_pedido = (pedido) => {
     
-    if (!remedio_correspondente) {
-      alert("Erro: Não foi possível encontrar o remédio deste pedido para estornar o estoque.");
-      return;
-    }
+    console.log(`Solicitando cancelamento do pedido ${pedido.id}...`);
+    //Guarda pedido a ser deletado
+    set_deleta(pedido);
+    //Confirmação
+    set_confirmar(true);
+    
+  };
+
+  const executa_delete = async() => {
+    console.log(`Confirmado: Pedido a ser deletado ${deleta.id}`);
+    
+    const remedio_correspondente = remedios.find(
+      (r) => r.name === deleta.remedio_nome
+    );
+  
+
+  if (!remedio_correspondente){
+    toast.error('Erro: Não foi possível encontrar o remedio desse pedido estornar o estoque');
+    cancela();
+    return; 
+  }
 
     // 2. Calcula o estorno
     const remedio_id = remedio_correspondente.id;
@@ -176,13 +206,13 @@ function Farmacia_page() {
       await atualizar_estoque_api(remedio_id, novo_estoque);
 
       //DELETA O PEDIDO
-      await fetch(`${API_URL}/pedidos/${pedido.id}`, { method: 'DELETE' });
+      await fetch(`${API_URL}/pedidos/${deleta.id}`, { method: 'DELETE' });
 
       //ATUALIZA A TELA
-      alert('Pedido cancelado e estornado com sucesso!');
+      toast.success('Pedido cancelado e estornado com sucesso!');
 
       // Remove o pedido da lista de pedidos
-      set_pedidos(pedidos_antigos => pedidos_antigos.filter(p => p.id !== pedido.id));
+      set_pedidos(pedidos_antigos => pedidos_antigos.filter(p => p.id !== deleta.id));
 
       // Atualiza o estoque do remédio
       set_remedios(remedios_antigos => remedios_antigos.map(r => 
@@ -191,20 +221,25 @@ function Farmacia_page() {
 
     } catch (error) {
       console.error('Erro ao deletar pedido:', error);
-      alert('Falha ao cancelar o pedido. Tente novamente.');
+      toast.error('Falha ao cancelar o pedido. Tente novamente.');
+    } finally{
+      cancela();
     }
   };
 
+  const cancela = () =>{
+    set_confirmar(false);
+    set_deleta(null);
+  }
+
+  const pedidos_filtrados = perfil === 'gerente'
+    ? pedidos 
+    : pedidos.filter(p => p.email_cliente === email_logado); 
 
   //RETURN
   return (
     <>
-      <header>
-        <h1>Farmacia Popular v2.0</h1>
-        <h2>Catalogo de Remedios</h2>
-        <button onClick={logout} style={{backgroundColor:'red', color:'white', marginLeft:'20px'}}>Sair</button>
-
-      </header>
+      
 
       <main id='catalogo_remedios'>
         {remedios.map((remedio) => (
@@ -219,27 +254,33 @@ function Farmacia_page() {
       </main>
 
       {/*Mapeando pedidos */}
+      {perfil &&(
+        <>
+      
       <hr id='pedidos_hr' />
-      <h2 id='pedidos_titulo'>Meus Pedidos</h2>
+      <h2 id='pedidos_titulo'>{perfil === 'gerente' ? 'Painel de Pedidos' : 'Meus Pedidos'}</h2>
       <section id='lista_pedidos'>
-        {pedidos.length === 0 ? (
+        {pedidos_filtrados.length === 0 ?(
           <p>Nenhum pedido encontrado.</p>
-        ) : (
-          pedidos.map((pedido) => (
-            <div key={pedido.id} className='pedido_card'>
-              <h4>Pedido de: {pedido.nome_cliente}</h4>
-              <p>Email: {pedido.email_cliente}</p>
-              <hr />
-              <p><strong>Remedio: </strong>{pedido.remedio_nome}</p>
-              <p><strong>Preço: </strong>{pedido.remedio_preco}</p>
-              
-              {/* ATUALIZADO: Botão de deletar agora funciona */}
-              <button onClick={() => deletar_pedido(pedido)}>Cancelar pedido</button>
+        ) :(
+          pedidos_filtrados.map ((pedidos)=>(
+            <div key={pedidos.id} className='pedido_card'>
+              <h4>Pedidos de: {pedidos.nome_cliente}</h4>
+              <p>Email: {pedidos.email_cliente}</p>
+              <hr/>
+              <p><strong>Remedio: </strong>{pedidos.remedio_nome}</p>
+              <p><strong>Preço: </strong>{pedidos.remedio_preco}</p>
+
+              {perfil === 'gerente' && (
+                <button onClick={() => deletar_pedido(pedidos)}> Cancelar Pedido </button>
+              )}
             </div>
           ))
         )}
-      </section>
-
+        </section>
+        </>
+      )}  
+      
       {/*Renderização do modal*/}
       {abrir_modal && (
         <div id='modal_pedidos_overlay'>
@@ -261,9 +302,24 @@ function Farmacia_page() {
             <button type='button' id='cancela_pedido' onClick={fechar_form}>
               Cancelar pedido
             </button>
-          </form>
+             </form>
         </div>
       )}
+
+          {confirmar &&(
+            <div id='modal_pedidos_overlay'>
+              <div id='modal_confirma_delete'>
+                <h3>Confirma Cancelamento</h3>
+                <p>Tem certeza que deseja cancelar este pedido?O item voltará ao estoque</p>
+                <div className='botoes_confirma'>
+                  <button id='botao_sim' onClick={executa_delete}>Sim</button>
+                  <button id='botao_nao' onClick={cancela}>Não</button>
+                </div>
+              </div>
+            </div>
+          )}
+            
+         
     </>
   );
 }
