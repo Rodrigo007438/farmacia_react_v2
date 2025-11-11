383e5951584f0e8901d8442992ef6fb1aa6d8d60
Rodrigo <joserodrigoguimaraes160401@gmail.com>
2025-11-11 15:46:42 -0300

    Update login_page.jsx

--- src/page/login_page.jsx
+++ src/page/login_page.jsx
@@ -4,6 +4,8 @@ import { Navigate, useNavigate } from "react-router-dom";
 
 import {toast} from 'react-toastify';
 
+import { useAuth } from "../components/AuthContext";
+
 import '../App.css';
 
 function LoginPage(){
@@ -13,24 +15,21 @@ function LoginPage(){
     //Hook de navegação
     const navigate = useNavigate();
 
+    const {login} = useAuth();
+
     const enviar_login = (event) => {
         event.preventDefault();
 
         if(username ==='admin' && password === 'admin123'){
             toast.success('Login como gerente bem-sucedido');
-            localStorage.setItem('perfil_usuario', 'gerente');
-            
-            window.dispatchEvent(new Event('storageChange'));
+           
+            login('gerente');
             
             navigate('/'); //redireciona para a pagina principal
         }else if(username === 'cliente' && password === 'cliente123'){
             toast.success('Login como cliente bem-sucedido');
-            localStorage.setItem('perfil_usuario', 'cliente');
 
-            window.dispatchEvent(new Event('storageChange'));
-
-            localStorage.setItem('email_usuario' , 'cliente.teste@email.com');
-
+            login('cliente', 'cliente.teste@email.com');
             navigate('/');
         }else{
             toast.error('Usuario ou senha incorreto');
