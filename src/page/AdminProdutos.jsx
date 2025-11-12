f50bf2443bcb3e039ab2a0d2603c541642e820fd
Rodrigo <joserodrigoguimaraes160401@gmail.com>
2025-11-12 11:16:00 -0300

    Update AdminProdutos.jsx

--- src/page/AdminProdutos.jsx
+++ src/page/AdminProdutos.jsx
@@ -297,7 +297,7 @@ function AdminProdutos(){
                     </div>
                     <div className="form_checkbox">
                         <label htmlFor="promocao">Em promoção?</label>
-                        <input type="checkbox" ip='promoção' name="promocao" checked={form.data.promocao} onChange={trocando_form} />
+                        <input type="checkbox" ip='promoção' name="promocao" checked={form_data.promocao} onChange={trocando_form} />
 
                     </div>
 
