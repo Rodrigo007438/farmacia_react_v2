00dd2d0273946e035e586c89771c73127226a6d0
Rodrigo <joserodrigoguimaraes160401@gmail.com>
2025-11-13 09:00:40 -0300

    Update LojaPage.jsx

--- src/page/LojaPage.jsx
+++ src/page/LojaPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import '../App.css';
 import { toast } from "react-toastify";
 
-const API_URL = 'http://localhost:4000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 function LojaPage(){
   const [remedios, set_remedios] = useState([]);
