bda0167c90fe584cc994a7964752b70c0ac205a2
Rodrigo <joserodrigoguimaraes160401@gmail.com>
2025-11-13 13:48:18 -0300

    Update login_page.jsx

--- src/page/login_page.jsx
+++ src/page/login_page.jsx
@@ -1,74 +1,76 @@
-import { useState } from "react";
+    import { useState } from "react";
 
-import { Navigate, useNavigate, Link } from "react-router-dom";
+    import { Navigate, useNavigate, Link } from "react-router-dom";
 
-import {toast} from 'react-toastify';
+    import {toast} from 'react-toastify';
 
-const API_URL = import.meta.env.VITE_API_URL;
+    const API_URL = import.meta.env.VITE_API_URL;
 
-import { useAuth } from "../components/AuthContext";
+    import { useAuth } from "../components/AuthContext";
 
-import '../App.css';
+    import '../App.css';
 
-function LoginPage(){
-    const [email, set_email] = useState('');
-    const [senha, set_senha] = useState('');
+    function LoginPage(){
+        const [email, set_email] = useState('');
+        const [senha, set_senha] = useState('');
 
-    //Hook de navegação
-    const navigate = useNavigate();
+        //Hook de navegação
+        const navigate = useNavigate();
 
-    const {login} = useAuth();
+        const {login} = useAuth();
 
-    const enviar_login = async (event) => {
-        event.preventDefault();
+        const enviar_login = async (event) => {
+            event.preventDefault();
 
-        try{
-            const response = await fetch(`${API_URL}/auth/login`, {
-                method: 'POST',
-                headers: {'Content-Type': 'application.json'},
-                body: JSON.stringify({email, senha})
-            });
+            try{
+                console.log("Tentando logar em:", `${API_URL}/auth/login`);
 
-            const data = await response.json();
+                const response = await fetch(`${API_URL}/auth/login`, {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({email, senha})
+                });
 
-            if(response.ok){
-                toast.success(`Bem vindo ${data.nome}!`);
+                const data = await response.json();
 
-                login(data.perfil, data.email);
+                if(response.ok){
+                    toast.success(`Bem vindo ${data.nome}!`);
 
-                navigate('/');
-            }else{
-                toast.error(data.message || 'Email ou senha incorretos');
+                    login(data.perfil, data.email);
+
+                    navigate('/');
+                }else{
+                    toast.error(data.message || 'Email ou senha incorretos');
+                }
+            }catch(error){
+                console.error('Erro no login:', error);
+                toast.error('Erro ao conectar com o servidor.');
             }
-        }catch(error){
-            console.error('Erro no login:', error);
-            toast.error('Erro ao conectar com o servidor.');
-        }
-    };
-    return(
-        <>
-        
-        <main>
-            <section className='login_card'>
-                <form id='form_login' onSubmit={enviar_login}>
-                    <div>
-                        <label htmlFor='email'>Email:</label>
-                        <input type='text' id='email' placeholder="Digite seu email" value={email} onChange={(e) => set_email (e.target.value)} />
-                    </div>
-                    <div>
-                        <label htmlFor='password'>Senha</label>
-                        <input type='password' id='password' placeholder='Digite sua senha' value={senha} onChange={(e) => set_senha(e.target.value)}/>
-                    </div>
-                    <button type='submit'>Entrar</button>
+        };
+        return(
+            <>
+            
+            <main>
+                <section className='login_card'>
+                    <form id='form_login' onSubmit={enviar_login}>
+                        <div>
+                            <label htmlFor='email'>Email:</label>
+                            <input type='text' id='email' placeholder="Digite seu email" value={email} onChange={(e) => set_email (e.target.value)} />
+                        </div>
+                        <div>
+                            <label htmlFor='password'>Senha</label>
+                            <input type='password' id='password' placeholder='Digite sua senha' value={senha} onChange={(e) => set_senha(e.target.value)}/>
+                        </div>
+                        <button type='submit'>Entrar</button>
 
-                    <p style={{textAlign: 'center', marginTop: '15px'}}>
-                    Não tem conta? <Link to="/cadastro" style={{color: '#28a745', fontWeight: 'bold'}}>Cadastre-se</Link>
-                    </p>
-                </form>
-            </section>
-        </main>
-        </>
-    )
-}
+                        <p style={{textAlign: 'center', marginTop: '15px'}}>
+                        Não tem conta? <Link to="/cadastro" style={{color: '#28a745', fontWeight: 'bold'}}>Cadastre-se</Link>
+                        </p>
+                    </form>
+                </section>
+            </main>
+            </>
+        )
+    }
 
-export default LoginPage;
+    export default LoginPage;
